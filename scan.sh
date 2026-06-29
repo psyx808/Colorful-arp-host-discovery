f498b01644fd850294db4805d9f08ecb85107c01
psyx808 <108977932+psyx808@users.noreply.github.com>
2026-06-29 22:50:50 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,5 +1,10 @@
-	g='\033[92m' 
-	c='\033[96m' 
-	y='\033[97m' 
-	r='\033[0m' 
-	arp-scan --localnet --resolve -F '${name}\t${ip}\t${mac}\t${vendor}' | awk -v g="$g" -v c="$c" -v y="$y" -v r="$r" 'NR > 2 {print g $1 r " ==> " y $2 r " " c $3 r " " substr($0, index($0, $4))}' | grep "192"
+scan () {
+        g='\033[92m'
+        c='\033[96m'
+        y='\033[97m'
+        r='\033[0m'
+        arp-scan --localnet --resolve --ignoredups --plain \
+                -F '${name}\t${ip}\t${mac}\t${vendor}' \
+                | awk -F '\t' -v g="$g" -v c="$c" -v y="$y" -v r="$r" \
+                        '!seen[$2 SUBSEP $3]++ {print g $1 r " ==> " y $2 r " " c $3 r " " $4}'
+}
